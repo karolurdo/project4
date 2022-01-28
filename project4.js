7f109dd251c12878a33f080af246866c703a1f3b
Urdo <karol.ugarenko@gmail.com>
2022-01-28 19:13:20 +0100

    add coments

--- project4.js
+++ project4.js
@@ -5,11 +5,12 @@ let orderElement = 1;
 const init = function() {
     const button = document.createElement('button'); // create button
     button.textContent = "Create elements"; // creacte textContent to button
-    button.style.fontSize = "20px"; // change fontSize 
+    button.style.fontSize = "20px"; // change fontSize
+    button.style.backgroundColor = "red";
     document.body.appendChild(button); // add button to body
-    const ul = document.createElement("ul");
-    document.body.appendChild(ul);
-    button.addEventListener('click', createLiElements);
+    const ul = document.createElement("ul"); // create ul 
+    document.body.appendChild(ul); // add ul to body 
+    button.addEventListener('click', createLiElements); // Listener
 
 };
 
