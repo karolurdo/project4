1740294dd38bfd99d3ecdd86b4d2450f3b2a4c65
Urdo <karol.ugarenko@gmail.com>
2022-01-28 23:06:35 +0100

    fix reset button

--- project4.js
+++ project4.js
@@ -4,6 +4,8 @@ let orderElement = 1;
 
 const init = function() {
     const button = document.createElement('button'); // create button
+    const resetButton = document.createElement('button');// reset button
+    document.body.appendChild(resetButton);
     button.textContent = "Create elements"; // creacte textContent to button
     button.style.fontSize = "20px"; // change fontSize
     button.style.backgroundColor = "red";
@@ -11,11 +13,8 @@ const init = function() {
     const ul = document.createElement("ul"); // create ul 
     document.body.appendChild(ul); // add ul to body 
     button.addEventListener('click', createLiElements); // Listener
-
-    const resetButton = document.createElement('button');
     resetButton.textContent = "Reset";
     resetButton.style.fontSize = "30px";
-    document.body.appendChild(resetButton);
     resetButton.addEventListener('click', reset);
 };
 
